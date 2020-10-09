deb69b3f6631c012f0fe35fa913c169aa4048a3f
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-10-09 12:54:59 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,18 +2,30 @@
 
 import math
 
-def tempConversion():
+def tempConversion(degree,unit="F"):
+    if unit=="F":
+        answer=(float(degree)-float(32))*float(5)/float(9)
+    if unit!="F":
+        answer=(float(degree)*(float(9)/float(5)))+float(32)
+    return answer
 
 
-def factorPair():
+def factorPair(num,factor):
+    answer=float(num)/float(factor),str(factor)
+    return answer
 
+def cosineLaw(a,b,,C):
+    answer=math.sqrt(float(a)**2+float(b)**2-2*float(a)*float(b)*cos(c))
 
-
-def cosineLaw():
 
 def convertAngle():
 
-def solution():
+def solution(a,b,c):
+    x1=(-float(b)+math.sqrt(float(b)**2-4*float(a)*float(c)))/2*float(a)
+    x2=(-float(b)-math.sqrt(float(b)**2-4*float(a)*float(c)))/2*float(a)
+    answer=[x1,x2]
+    answer.sort()
+    return answer
 
-def quadratic():
+#def quadratic():
 
