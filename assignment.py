8a1be90d631fd64168b49768e8ea7a867ba4bef0
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-10-13 09:31:52 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,30 +2,38 @@
 
 import math
 
-def tempConversion(degree,unit="F"):
-    if unit=="F":
-        answer=(float(degree)-float(32))*float(5)/float(9)
-    if unit!="F":
-        answer=(float(degree)*(float(9)/float(5)))+float(32)
+def tempConversion(degree, unit = "C"):
+    answer = round((9*float(degree)/5 + 32),1)
+    if unit == "F":
+        answer = round((float(degree) - 32)*5/9,1)
     return answer
 
-
-def factorPair(num,factor):
-    answer=float(num)/float(factor),str(factor)
-    return answer
-
-def cosineLaw(a,b,C):
-    answer=math.sqrt(float(a)**2+float(b)**2-2*float(a)*float(b)*cos(c))
-
-
-def convertAngle():
-
-def solution(a,b,c):
-    x1=(-float(b)+math.sqrt(float(b)**2-4*float(a)*float(c)))/2*float(a)
-    x2=(-float(b)-math.sqrt(float(b)**2-4*float(a)*float(c)))/2*float(a)
-    answer=[x1,x2]
+def factorPair(num,a):
+    b = int(num/a)
+    answer = [a,b]
     answer.sort()
     return answer
 
-#def quadratic():
+def convertAngle(degree):
+    angle = math.pi*float(degree)/180
+    return angle
+
+def quadratic(a,b,c):
+    x1 = round((-float(b) + math.sqrt(float(b)**2 - 4*float(a)*float(c)))/(2*float(a)),2)
+    x2 = round((-float(b) - math.sqrt(float(b)**2 - 4*float(a)*float(c)))/(2*float(a)),2)
+    solution = [x1,x2]
+    solution.sort()
+    return solution
+
+def solution(List):
+    if List[0] > 0:
+        answer = List[0]
+    elif List[1] > 0:
+        answer = List[1]
+    return answer
+
+
+print(convertAngle(30))
+print(solution([-8.9, 5.3]))
+print(quadratic(3,5,-8))
 
